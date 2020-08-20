ac18d74181b5d3b45f91d48c7d472e7a8a0d5b5a
Ikarosx <532288425@qq.com>
2020-08-21 07:50:03 +0800

    Repository补上@Param

--- src/main/java/cn/ikarosx/homework/repository/ManageClassRepository.java
+++ src/main/java/cn/ikarosx/homework/repository/ManageClassRepository.java
@@ -5,6 +5,7 @@ import cn.ikarosx.homework.model.BO.ManageClassDetailInfoUser;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 /**
  * @author Ikarosx
@@ -14,5 +15,5 @@ public interface ManageClassRepository extends JpaRepository<ManageClass, String
   @Query(
       value =
           "SELECT new cn.ikarosx.homework.model.BO.ManageClassDetailInfoUser(u.id, u.username, u.nickname, u.studentNo, cu.createTime) FROM User u LEFT JOIN ManageClassUser cu ON u.id = cu.userId WHERE cu.classId = :classId AND (:status IS NULL OR :status = cu.status)")
-  List<ManageClassDetailInfoUser> findAllByClassIdAndStatus(String classId, Integer status);
+  List<ManageClassDetailInfoUser> findAllByClassIdAndStatus(@Param("classId") String classId,@Param("status") Integer status);
 }
--- src/main/java/cn/ikarosx/homework/repository/ManageHomeworkRepository.java
+++ src/main/java/cn/ikarosx/homework/repository/ManageHomeworkRepository.java
@@ -6,6 +6,7 @@ import cn.ikarosx.homework.model.BO.ManageHomeworkDetails;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 /**
  * @author Ikarosx
@@ -19,9 +20,11 @@ public interface ManageHomeworkRepository extends JpaRepository<ManageHomework, 
 
   @Query(
       "SELECT new cn.ikarosx.homework.model.BO.ManageHomeworkDetails(h.id, h.title, h.description, h.deadline, CASE hu.status WHEN 1 then true ELSE false END, h.createTime, h.updateTime) FROM ManageHomework h LEFT JOIN ManageHomeworkUser hu ON hu.homeworkId = h.id AND hu.userId = :id WHERE h.classId = :classId")
-  List<ManageHomeworkDetails> listAllManageHomeworksByCurrentUser(String id, String classId);
+  List<ManageHomeworkDetails> listAllManageHomeworksByCurrentUser(
+      @Param("id") String id, @Param("classId") String classId);
 
   @Query(
       "SELECT new cn.ikarosx.homework.model.BO.HomeworkFinishInfo(u.nickname, u.studentNo, hu.status, hu.createTime) FROM User u LEFT JOIN  ManageHomeworkUser hu ON hu.userId = u.id AND hu.homeworkId = :homeworkId WHERE u.classId = :classId ORDER BY hu.status, u.studentNo")
-  List<HomeworkFinishInfo> getHomeworkFinishInfo(String classId, String homeworkId);
+  List<HomeworkFinishInfo> getHomeworkFinishInfo(
+      @Param("classId") String classId, @Param("homeworkId") String homeworkId);
 }
