ca9964250efef85c7b2898de13704d7a6ea9792e
Ikarosx <532288425@qq.com>
2020-09-19 10:35:59 +0800

    修复提交作业与数据库连接时区问题

--- src/main/java/cn/ikarosx/homework/entity/ManageHomeworkUser.java
+++ src/main/java/cn/ikarosx/homework/entity/ManageHomeworkUser.java
@@ -1,18 +1,19 @@
 package cn.ikarosx.homework.entity;
-import lombok.Data;
-
-import java.io.Serializable;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import java.io.Serializable;
 import java.util.Date;
-import javax.validation.constraints.NotNull;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Data;
 import org.hibernate.annotations.GenericGenerator;
-import cn.ikarosx.homework.entity.jpa.Update;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
-import javax.persistence.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 /**
  * @author Ikarosx
@@ -26,84 +27,73 @@ import javax.persistence.*;
 @ApiModel(value = "ManageHomeworkUser")
 public class ManageHomeworkUser implements Serializable {
 
-	@Id
-	@GeneratedValue(generator = "uuid")
-	@ApiModelProperty(value = "用户作业ID")
-	private String id;
+  @Id
+  @GeneratedValue(generator = "uuid")
+  @ApiModelProperty(value = "用户作业ID")
+  private String id;
 
-	@ApiModelProperty(value = "用户ID")
-	private String userId;
+  @ApiModelProperty(value = "用户ID")
+  private String userId;
 
-	@ApiModelProperty(value = "作业ID")
-	private String homeworkId;
+  @ApiModelProperty(value = "作业ID")
+  private String homeworkId;
 
-	@ApiModelProperty(value = "内容")
-	private String content;
+  @ApiModelProperty(value = "0未完成 1完成")
+  private Integer status;
 
-	@ApiModelProperty(value = "0未完成 1完成")
-	private Integer status;
+  @CreatedDate
+  @ApiModelProperty(value = "创建时间", hidden = true)
+  private Date createTime;
 
-	@CreatedDate
-	@ApiModelProperty(value = "创建时间",hidden = true)
-	private Date createTime;
+  @LastModifiedDate
+  @ApiModelProperty(value = "更新时间", hidden = true)
+  private Date updateTime;
 
-	@LastModifiedDate
-	@ApiModelProperty(value = "更新时间",hidden = true)
-	private Date updateTime;
+  public String getId() {
+    return id;
+  }
 
-	public String getId() {
-		return id;
-	}
+  public void setId(String id) {
+    this.id = id;
+  }
 
-	public void setId(String id) {
-		this.id = id;
-	}
+  public String getUserId() {
+    return userId;
+  }
 
-	public String getUserId() {
-		return userId;
-	}
+  public void setUserId(String userId) {
+    this.userId = userId;
+  }
 
-	public void setUserId(String userId) {
-		this.userId = userId;
-	}
+  public String getHomeworkId() {
+    return homeworkId;
+  }
 
-	public String getHomeworkId() {
-		return homeworkId;
-	}
+  public void setHomeworkId(String homeworkId) {
+    this.homeworkId = homeworkId;
+  }
 
-	public void setHomeworkId(String homeworkId) {
-		this.homeworkId = homeworkId;
-	}
+  public Integer getStatus() {
+    return status;
+  }
 
-	public String getContent() {
-		return content;
-	}
+  public void setStatus(Integer status) {
+    this.status = status;
+  }
 
-	public void setContent(String content) {
-		this.content = content;
-	}
+  public Date getCreateTime() {
+    return createTime;
+  }
 
-	public Integer getStatus() {
-		return status;
-	}
+  public void setCreateTime(Date createTime) {
+    this.createTime = createTime;
+  }
 
-	public void setStatus(Integer status) {
-		this.status = status;
-	}
+  public Date getUpdateTime() {
+    return updateTime;
+  }
 
-	public Date getCreateTime() {
-		return createTime;
-	}
-
-	public void setCreateTime(Date createTime) {
-		this.createTime = createTime;
-	}
-
-	public Date getUpdateTime() {
-		return updateTime;
-	}
-
-	public void setUpdateTime(Date updateTime) {
-		this.updateTime = updateTime;
-	}
+  public void setUpdateTime(Date updateTime) {
+    this.updateTime = updateTime;
+  }
 }
